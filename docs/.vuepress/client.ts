328fb8f14a467b63e7d6d6363947ab24168f0832
lizhimou <lizhimou@bytedance.com>
2025-11-24 17:57:15 +0800

    update

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,19 +1,20 @@
 import { defineClientConfig } from "vuepress/client";
 import typedContent from "./components/typedContent.vue";
+import { RefSymbol } from "@vue/reactivity";
 
 export default defineClientConfig({
   // 客户端增强
-  enhance: ({ app, router }) => {
+  enhance: ({ app, router,siteData }) => {
     // 注册全局组件
     app.component("typedContent", typedContent);
 
     // 注册路由守卫 打印日志
     router.beforeEach((to) => {
-      console.log("before navigation"+"-"+to.fullPath);
+      console.log("before navigation"+"-"+decodeURIComponent(to.fullPath));
     });
 
     router.afterEach((to) => {
-      console.log("after navigation"+"-"+to.fullPath);
+      console.log("after navigation"+"-"+decodeURIComponent(to.fullPath));
     });
   },
 
