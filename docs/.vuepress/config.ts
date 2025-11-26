1594a303c34be75178669b9a51989d908d633647
lizhimou <lizhimou@bytedance.com>
2025-11-26 11:30:59 +0800

    shiki styling

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,8 @@
 import { defineUserConfig } from "vuepress";
 import { sitemapPlugin } from '@vuepress/plugin-sitemap'
+import { appendDatePlugin } from '@vuepress/plugin-append-date'
+import { shikiPlugin } from '@vuepress/plugin-shiki'
+
 
 import theme from "./theme.js";
 
@@ -28,6 +31,14 @@ export default defineUserConfig({
     sitemapPlugin({
       hostname: "aaronlzm.github.io",
       changefreq: "daily",
+    }),
+    appendDatePlugin({
+      format: "full",
+      key: "full"
+    }),
+    shikiPlugin({
+      // options
+      langs: ['ts', 'json', 'vue', 'md', 'bash', 'diff', "python","py"],
     }),
   ],
 });
