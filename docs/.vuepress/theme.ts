1444e1e5d3ef52f67d05a2c5a541b59284510b57
Zhimou(Aaron) Li <lzm_aaron@outlook.com>
2026-05-25 13:40:16 +0800

    deps: update dependencies + remove P0 issues (fuck script, hardcoded plaintext password)

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -76,18 +76,7 @@ export default hopeTheme({
     },
   },
 
-  encrypt: {
-    config: {
-      "/demo/encrypt.html": {
-        hint: "Password: 1234",
-        password: "1234",
-      },
-      "/zh/demo/encrypt.html": {
-        hint: "Password: 1234",
-        password: "1234",
-      },
-    },
-  },
+
 
   // enable it to preview all changes in time
   // hotReload: true,
